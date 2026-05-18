1879eaa945ea408f3bec1df663cf502b4eefcfbd
Dharshan220 <dharshane21@gmail.com>
2026-05-18 09:22:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({base:'/resum/',//add the line right here!
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
